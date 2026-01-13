1709b747bfc070594c3b6333a3b3bb32bbe7a224
istvan-tamas <istvan.tamas90@gmail.com>
2026-01-13 16:26:33 +0100

    Added timing to densenet201

--- models/densenet_201.py
+++ models/densenet_201.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import torch
 from torch import nn
 from torch.optim import AdamW
@@ -139,7 +140,9 @@ def train_one_fold(fold_idx, train_ds, val_ds):
     return best_acc
 
 
-for fold in range(5):
+start = datetime.now() #timing!
+
+for fold in range(NUM_FOLDS):
     print(f"\n===== Fold {fold} =====")
 
     train_ds = LMDBDataset(
@@ -153,3 +156,5 @@ for fold in range(5):
     )
 
     train_one_fold(fold, train_ds, val_ds)
+    
+print("Training completed in: " + str(datetime.now() - start))
