9977ad0b4788ab023d9f6393579a3a7a4a195d46
istvan-tamas <istvan.tamas90@gmail.com>
2026-01-13 16:29:40 +0100

    Added timing to inception resnet v2

--- models/inception_resnet_v2.py
+++ models/inception_resnet_v2.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import torch
 import timm
 import torch
@@ -125,6 +126,8 @@ def train_one_fold(fold_idx, train_ds, val_ds):
 
     return best_acc
 
+start = datetime.now() #timing!
+
 for fold in range(NUM_FOLDS):
     print(f"\n===== Fold {fold} =====")
 
@@ -136,3 +139,5 @@ for fold in range(NUM_FOLDS):
     )
 
     train_one_fold(fold, train_ds, val_ds)
+    
+start = datetime.now() #timing!
